36f966257f4e7b663ad93f18e6591fae9627ddcb
Matheus Baptistella <mbapts@gmail.com>
2026-02-25 16:03:39 -0300

    handle dir creation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::fs::OpenOptions;
+
+use directories::BaseDirs;
+
 fn main() {
-    println!("Hello, world!");
+    let base_dirs = BaseDirs::new().unwrap();
+    let app_dir = base_dirs.data_local_dir().join("habit-rs");
+    std::fs::create_dir_all(&app_dir).unwrap();
+
+    let app_data = app_dir.join("data.json");
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(app_data)
+        .unwrap();
+}
+
+
+enum HabitStatus {
+    Completed,
+    Todo
+}
+
+struct Habit {
+    color: u32,
+    status: HabitStatus,
 }
